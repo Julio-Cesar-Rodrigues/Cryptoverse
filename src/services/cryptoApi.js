c44927525e6c59fed3f7bd1668879918cee1a45a
Julio César Rodrigues <62729892+Julio-Cesar-Rodrigues@users.noreply.github.com>
2022-01-19 14:24:03 -0300

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaaders = {
   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-  'x-rapidapi-key': '605a9c597emshcc9c2b105bc6969p1a2eb4jsn03561adc1c90'
+  'x-rapidapi-key': 'KEY_RAPID_API'
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com'
@@ -21,22 +21,15 @@ export const cryptoApi = createApi({
       query: coinId => createRequest(`/coin/${coinId}`)
     }),
 
-    // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
     getCryptoHistory: builder.query({
       query: ({ coinId, timeperiod }) =>
         createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`)
     }),
-
-    // Note: To access this endpoint you need premium plan
-    getExchanges: builder.query({
-      query: () => createRequest('/exchanges')
-    })
   })
 })
 
 export const {
   useGetCryptosQuery,
   useGetCryptoDetailsQuery,
-  useGetExchangesQuery,
   useGetCryptoHistoryQuery
 } = cryptoApi
